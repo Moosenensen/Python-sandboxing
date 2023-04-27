a75b7d52fb993be3d63c62987ddf2dafeff1df91
Moosenensen <106861957+Moosenensen@users.noreply.github.com>
2023-04-27 18:15:41 +1000

    Add files via upload

---
+++ bignumbercalc.py
@@ -0,0 +1,18 @@
+import math
+
+def numlength(a):
+    length = math.floor(math.log(a,10))+1
+    return length
+
+def printlist(a):
+    for i in lista:
+        print(i,end='')
+def makelist(a):
+    for i in range(1,numlength(a)+1):
+        math.floor(a)
+
+
+intnumb = int(input("test code "))
+intnumb = numlength(intnumb)
+lista = [1,2,3,4]
+makelist(lista)
