a7bbf83e1011a98cfdee7358e75c68e48d25437a
Moosenensen <hunter@franklinhome.net.au>
2023-05-05 19:50:43 +1000

    num add works added num size comp

--- bignumbercalcdeciamal.py
+++ bignumbercalcdeciamal.py
@@ -1,7 +1,7 @@
 import math
 def listcon(num,list):
     f = 0
-    if num[0] != '-' or num[0] != '+':
+    if num[0] != '-' and num[0] != '+':
         list.append('+')
     for i in num:
         try:
@@ -37,7 +37,8 @@ def numcon(list,a):
     if d == 1:
         a = a * -1
     return a
-def listlineup(lista, listb):
+def llineup(lista, listb):
+    
     for i in range(1, len(lista)+1 ):
             if lista[-i] == '.': 
                 a = i
@@ -61,25 +62,22 @@ def listlineup(lista, listb):
     deltlen = lena - lenb
     if deltlen > 0:
         for i in range(deltlen):
-            listb.insert(0,0)
+            listb.insert(1,0)
         
     elif deltlen < 0:
         for i in range(deltlen*-1):
-            lista.insert(0,0)
-    lista.insert(0,0)
-    listb.insert(0,0)
-    return(lista, listb)    
+            lista.insert(1,0)
+    
+    return(lista, listb)  
+ 
 
-def listadd(a,b,c):
-    print(a)
-    print(b)
-    printlist(a)
-    printlist(b)
-    for i in range(1, len(a)+1):
-        printlist(c)
-        print(str(a[i]) + ' ' + str(b[i]))
+def ladd(a,b,c):
+    llineup(a,b)
+    c.append(0)
+    for i in range(1, len(a)):
         
-        try:
+        
+        if a[i] != '.':
             if a[i]+b[i] < 10:
                 c.append(a[i]+b[i])
             else:
@@ -92,23 +90,49 @@ def listadd(a,b,c):
                     c.insert(i-1,c[i-2]+1)
                     c.pop(i-2)
                     c.append((a[i]+b[i]-10))
-        except:
+        else:
             
                 c.append('.')
     if c[0] == 0:
         c.pop(0)
+    return(c)
+def lscmp(a,b):
+    llineup(a,b)
+    sign = 0
+    for i in range(1, len(a)):
+        
+        
+        if a[i] != '.':
+            
+            if a[i]-b[i] != 0:
+
+                if a[i]-b[i] <= 0:
+                    sign = 1
+                    break 
+                else:
+                    sign = 0
+                    break
+    return(sign)
+
+            
+    
+def lsub (a,b,c):
+    llineup(a,b)
 
 
 
 
 
 
-number = input("num ")
-number2 = input("num ")
+
+#number = input("num ")
+#number2 = input("num ")
+number = "66666.1"
+number2 = "6666.1732"
 a = []
 b = []
 c = []
 listcon(number,a)
 listcon(number2,b)
-printlist(a)
-listadd(a,b,c)
+
+print(lscmp(a,b))
